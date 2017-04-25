d3a3f00cc7b2b2a7702c5ec60b4e281f3faa42a2
actboy168 <actboy168@gmail.com>
2017-04-25 13:59:07 +0800

    hash使用c计算

--- tools/mapdump/lua/test.lua
+++ tools/mapdump/lua/test.lua
@@ -1,37 +1,15 @@
 require 'filesystem'
 local uni = require 'ffi.unicode'
-local stormlib = require 'ffi.stormlib'
-
-local io_open = io.open
-function io.open(path, ...)
-    return io_open(uni.u2a(path:string()), ...)
-end
-function io.load(filepath)
-	local f, e = io.open(filepath, 'rb')
-	if f then
-		local content = f:read('*a')
-		f:close()
-		return content
-	else
-		return nil, e
-	end
-end
 
 local output = fs.path(arg[1])
 local input  = fs.path(arg[2])
 local jass   = fs.path(arg[3])
 
 local t = os.clock()
-local map = stormlib.open(input, true)
-if not map then
-    error('Couldn\'t open map.')
-    return
-end
 
-local of = io.open(output, 'wb')
+local of = io.open(uni.u2a(output:string()), 'wb')
 if not of then
-    error(e)
-    return
+    return error(e)
 end
 
 local function write(s)
@@ -39,51 +17,41 @@ local function write(s)
     print(s)
 end
 
-local function tohex(n, reverse)
-    if reverse then
-	    local s = tostring(n & 255)
-	    n = n // 256
-	    s = tostring(n & 255) .. ' ' .. s
-	    n = n // 256
-	    s = tostring(n & 255) .. ' ' .. s
-	    n = n // 256
-	    return tostring(n & 255) .. ' ' .. s
-    end
-	local s = tostring(n & 255)
-	n = n // 256
-	s = s .. ' ' .. tostring(n & 255)
-	n = n // 256
-	s = s .. ' ' .. tostring(n & 255)
-	n = n // 256
-	return s .. ' ' .. tostring(n & 255)
+local function tohex(n)
+    return (n & 0xFF)
+    ..' '..((n >> 8) & 0xFF)
+    ..' '..((n >> 16) & 0xFF)
+    ..' '..((n >> 24) & 0xFF)
 end
 
--- Step.1 基本信息
-do 
-    local crc32 = require 'crc32'
-    local buf = io.load(input)
-    write('map_size = ' .. tohex(#buf))
-    write('map_info = ' .. tohex(crc32(buf)))
+local function tohex2(n)
+    return (n & 0xFF)
+    ..' '..((n >> 8) & 0xFF)
 end
 
--- Step.2 hash值计算
 do
-    local hash = require 'hash'
-    local crc, sha1_a, sha1_b, sha1_c, sha1_d, sha1_e = hash(jass, map)
+    local maphash = require 'maphash'
+    local size, info, crc, sha1_a, sha1_b, sha1_c, sha1_d, sha1_e = maphash(input, jass)
+    write('map_size = ' .. tohex(size))
+    write('map_info = ' .. tohex(info))
     write('map_crc = ' .. tohex(crc))
-    write('map_sha1 = ' .. tohex(sha1_a, true)..' '..tohex(sha1_b, true)..' '..tohex(sha1_c, true)..' '..tohex(sha1_d, true)..' '..tohex(sha1_e, true))
+    write('map_sha1 = ' .. tohex(sha1_a)..' '..tohex(sha1_b)..' '..tohex(sha1_c)..' '..tohex(sha1_d)..' '..tohex(sha1_e))
 end
 
--- Step.3 w3i信息
 do
+    local stormlib = require 'ffi.stormlib'
+    local map = stormlib.open(input, true)
+    if not map then
+        return error('Couldn\'t open map.')
+    end
     local w3i = require 'w3i'
     local info = w3i(map:load_file 'war3map.w3i')
     local map_options = (info['选项']['对战地图'] << 2)
         | (info['选项']['自定义玩家分组'] << 5)
         | (info['选项']['自定义队伍'] << 6)
     write('map_options = ' .. map_options)
-    write('map_width = ' .. tohex(info['地形']['地图宽度']))
-    write('map_height = ' .. tohex(info['地形']['地图长度']))
+    write('map_width = ' .. tohex2(info['地形']['地图宽度']))
+    write('map_height = ' .. tohex2(info['地形']['地图长度']))
     local n = info['玩家']['玩家数量']
     local closed = 0
     local players = {}
